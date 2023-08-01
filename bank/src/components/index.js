b4c70c925a1daad5b69ece793ec9253869cc357f
cristinaboris <cristina.boris@ukr.net>
2023-08-01 23:17:32 +0300

    MainProject
    MainProject

---
+++ bank/src/components/index.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import CTA from './CTA'
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import Hero from './Hero';
+import Stats from './Stats'
+import GetStarted from './GetStarted';
+import Client from './Client'
+import Testimonials from './Testimonials'
+
+
+export {Navbar, CTA, Billing, Business, Button, CardDeal, FeedbackCard, Footer, Hero, Stats, GetStarted,Client, Testimonials }
